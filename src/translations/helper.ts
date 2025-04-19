a6db9b76d233f70122fee871e330c7b173863eee
Joshua Coles <me@joshuacoles.me>
2025-04-19 16:18:41 +0100

    Improve type checking for translations

--- src/translations/helper.ts
+++ src/translations/helper.ts
@@ -1,6 +1,7 @@
 // Modern translation system for Obsidian plugins
 import { moment } from "obsidian";
-import { translationManager } from "./manager";
+import { type TranslationManager, translationManager } from "./manager";
+import type { TranslationOptions } from "./types";
 export type { TranslationKey } from "./types";
 
 // Initialize translations
@@ -10,4 +11,4 @@ export async function initializeTranslations(): Promise<void> {
 }
 
 // Export the translation function
-export const t = translationManager.t.bind(translationManager);
+export const t: TranslationManager['t'] & ((key: string, options?: TranslationOptions) => string) = translationManager.t.bind(translationManager);
